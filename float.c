63803e9190dafc0e9590dfbc3f1860f65dda2c09
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 10:17:51 +0700

    Add files via upload

---
+++ float.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ main()
+ {
+ 	int a = 10;
+ 	int b = a+a++;
+ 	int x = -5;
+ 	int y = -x;
+ 	float num = 23.34;
+ 	printf("num = %f\n", num);
+ 	printf(" b = %d\n", b);
+ 	printf(" y = %d", y);
+ 	return 0;
+ }
